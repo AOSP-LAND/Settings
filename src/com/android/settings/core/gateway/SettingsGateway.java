Fabulous-Oreo/packages_apps_Settings

[SQUASHED] Implement Button Settings
commit 72f23ad59276b0efdb1689f6d4751b69f960c901
Author: maxwen <max.weninger@gmail.com>
Date:   Thu Aug 31 23:16:42 2017 +0800

    Add custom NitrogenSettings dashboard
    Adapt to ButtonSettings

    Change-Id: I648ffdd28982cba167446d6f7ee2f761642a5032

commit 11a60d4e2b365c6e5789e987a7a73e612a9e702c
Author: xyyx <xyyx@mail.ru>
Date:   Mon Sep 11 10:30:22 2017 +0800

    Buttons: Add buttons brightness and button backlight timeout options

commit 16946ce4df28f9ae44541f66b80264b92244c0d5
Author: xyyx <xyyx@mail.ru>
Date:   Thu Sep 14 22:55:23 2017 +0800

    TEMP: Hardware key custom rebinding (2/2)

commit 48d88ee94f23dc0b5f1d724a3e12b14b41882b1f
Author: xyyx <xyyx@mail.ru>
Date:   Fri Sep 15 08:56:18 2017 +0800

    HW keys: Add screenshot,partial screenshot, pip actions

commit c0ab813faac426a3bc5337d046e4c5dcf73440a7
Author: xyyx <xyyx@mail.ru>
Date:   Fri Sep 15 09:33:53 2017 +0800

    HW keys tweaks (2/2)

Change-Id: Ia649384ea1e5f34a39619246ad589315db4a21ff
Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -127,6 +127,8 @@ import com.android.settings.wifi.WifiInfo;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
+import com.android.settings.custom.fragments.ButtonSettings;
+
 public class SettingsGateway {
 
     /**
@@ -244,6 +246,7 @@ public class SettingsGateway {
             EnterprisePrivacySettings.class.getName(),
             WebViewAppPicker.class.getName(),
             LockscreenDashboardFragment.class.getName(),
+            ButtonSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -281,5 +284,6 @@ public class SettingsGateway {
             Settings.DateTimeSettingsActivity.class.getName(),
             Settings.DeviceInfoSettingsActivity.class.getName(),
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
+            Settings.ButtonSettingsActivity.class.getName(),
     };
 }

AOSP-LAND/Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -132,6 +132,8 @@ import com.android.settings.wifi.WifiInfo;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
+import com.android.settings.custom.fragments.ButtonSettings;
+
 public class SettingsGateway {
 
     /**
@@ -252,7 +254,8 @@ public class SettingsGateway {
             WebViewAppPicker.class.getName(),
             LockscreenDashboardFragment.class.getName(),
             BluetoothDeviceDetailsFragment.class.getName(),
-            DataUsageList.class.getName()
+            DataUsageList.class.getName(),
+            ButtonSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -290,5 +293,6 @@ public class SettingsGateway {
             Settings.DateTimeSettingsActivity.class.getName(),
             Settings.DeviceInfoSettingsActivity.class.getName(),
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
+            Settings.ButtonSettingsActivity.class.getName(),
     };
 }